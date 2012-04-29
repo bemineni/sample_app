e0ae38caf4e0bb158d2190f42f9ae354e607c57e
Srikanth Bemineni <srikanth.bemineni@gmail.com>
2012-04-29 18:37:03 -0500

    finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Srikanth Bemineni"
+    email    "srikanth.bemineni@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
+
